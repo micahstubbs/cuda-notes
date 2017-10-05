6156189086cccb56c2d99c86b8e42c47450ee9cd
Micah Stubbs <micah.stubbs@gmail.com>
2017-10-04 21:25:35 -0700

    add log of attempted theano cuda setup

---
+++ theano.sh
@@ -0,0 +1,9 @@
+# followed the guide here
+# http://deeplearning.net/software/theano/install_ubuntu.html
+
+# to install Theano 0.9.0
+
+# then installed CUDA 8 and cuDNN 6
+
+# then For cuda 8, the dev version of skcuda (will be released as 0.5.2) is needed for cusolver: 
+pip install pycuda; pip install git+https://github.com/lebedov/scikit-cuda.git#egg=scikit-cuda
